cb9543c8b6bc581d23908110f5fcfa4f1f982d48
RaidenDilan <raiden18@me.com>
2020-09-01 23:06:00 +0100

    Added withRouter to cart-dropdown + Created checkout page and route + updated checkout page using selectors with reselect

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Checkout from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -52,6 +53,10 @@ class App extends React.Component {
           <Route
             path='/shop'
             component={ ShopPage } />
+          <Route
+            exact
+            path='/checkout'
+            component={ Checkout } />
           <Route
             exact
             path='/signin'
