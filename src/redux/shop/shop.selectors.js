7c49c73aa6a6fdd00e16a766ae77ccfb6ad17c69
RaidenDilan <raiden18@me.com>
2020-09-06 00:20:19 +0100

    Created WithSpinner HOC (Higher-Order-Component)

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -10,10 +10,12 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key]) // Get all the keys and map over the array of keys to get the value of each key, which will give us an array of our items.
+  collections => collections
+    ? Object.keys(collections).map(key => collections[key]) // Get all the keys and map over the array of keys to get the value of each key, which will give us an array of our items.
+    : []
 );
 
 export const selectCollection = memoize((collectionUrlParam => createSelector(
   [selectCollections],
-  collections => collections[collectionUrlParam]
+  collections => (collections ? collections[collectionUrlParam] : null)
 )));
