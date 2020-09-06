830c8cfed00e95d5748eed49f4e32e76018d44cb
RaidenDilan <raiden18@me.com>
2020-09-06 23:41:39 +0100

    Sign Out With Sagas

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,12 +7,13 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crwn.svg';
 
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionsLink } from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to='/'>
       <Logo className='logo' />
@@ -26,10 +27,14 @@ const Header = ({ currentUser, hidden }) => (
       </OptionsLink>
       {
         currentUser
-          ? (<OptionsLink
-            as='div'
-            onClick={ () => auth.signOut() }>SIGNOUT</OptionsLink>)
-          : (<OptionsLink to='/signin'>SIGNIN</OptionsLink>)
+          ? (
+            <OptionsLink
+              as='div'
+              onClick={ signOutStart }>SIGNOUT</OptionsLink>
+          )
+          : (
+            <OptionsLink to='/signin'>SIGNIN</OptionsLink>
+          )
       }
       <CartIcon />
     </OptionsContainer>
@@ -42,4 +47,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { signInSuccess, signInFailure } from './user.actions';
+import { signInSuccess, signInFailure, signOutSuccess, signOutFailure } from './user.actions';
 
 import { auth, googleProvider, createUserProfileDocument, getCurrentUser } from '../../firebase/firebase.utils';
 
@@ -56,10 +56,24 @@ export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield (put(signOutSuccess()));
+  } catch (error) {
+    yield put(signOutFailure(error));
+  }
+}
+
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSaga() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
-    call(onCheckUserSession)
+    call(onCheckUserSession),
+    call(onSignOutStart)
   ]);
 }
