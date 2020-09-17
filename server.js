6de7d404e8aa9015ce71a82a62ea17645d1b47e1
RaidenDilan <raiden18@me.com>
2020-09-17 20:29:21 +0100

    Updating app code to be pwa compliant + Added serviceWorker + https + updated Manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -11,20 +12,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) return false
-  return compression.filter(req, res);
-}
+app.use(compression());
 
-app.use(compression({
-  filter: shouldCompress,
-  level: 7
-}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true })); // trustProtoHeader => Heroku runs a reverse-proxy
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -35,6 +30,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+app.get('/serviceWorker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'serviceWorker.js'));
 });
 
 app.post('/payment', (req, res) => {
